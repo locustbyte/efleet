ba9eb9ab0bf9317fe62ca1cc2a80c35e6ccb0448
Locustbyte <locustbyte@me.com>
2020-01-25 04:22:22 +0000

    up

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('eFleet', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'mgcrea.ngStrap', 'ui.mask']);
+    .module('eFleet', ['ngAnimate', 'ngResource', 'ui.router', 'ui.bootstrap', 'mgcrea.ngStrap', 'ui.mask']);
 
 })();
